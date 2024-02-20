9fa644d0755bc0997bb3052b875fb16d8487f50b
Andrey Korolev <andrey.korolev@gmail.com>
2024-02-20 20:23:36 +0300

    added

---
+++ Handler.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Task8
+{
+	interface IHandler
+	{
+		TimeSpan Timeout { get; }
+
+		Task PerformOperation(CancellationToken cancellationToken);
+	}
+
+	class Handler : IHandler
+	{
+		private readonly IConsumer _consumer;
+		private readonly IPublisher _publisher;
+		private readonly ILogger<Handler> _logger;
+
+		public TimeSpan Timeout { get; }
+
+		public Handler(TimeSpan timeout, IConsumer consumer, IPublisher publisher, ILogger<Handler> logger)
+		{
+			Timeout = timeout;
+
+			_consumer = consumer;
+			_publisher = publisher;
+			_logger = logger;
+		}
+
+		public async Task PerformOperation(CancellationToken cancellationToken)
+		{
+			do
+			{
+				var data = await _consumer.ReadData();
+				var payload = data.Payload;
+
+				foreach (var recipient in data.Recipients)
+				{
+					var tr = Task.Run(async () =>
+					{
+						bool finished = false;
+
+						while (!finished)
+						{
+							var res = await _publisher.SendData(recipient, payload);
+
+							if (res == SendResult.Rejected)
+								await Task.Delay(Timeout, cancellationToken);
+							else
+								finished = true;
+						}
+					});
+				}
+			} while (!cancellationToken.IsCancellationRequested);
+		}
+	}
+
+	record Payload(string Origin, byte[] Data);
+	record Address(string DataCenter, string NodeId);
+	record Event(IReadOnlyCollection<Address> Recipients, Payload Payload);
+
+	enum SendResult
+	{
+		Accepted,
+		Rejected
+	}
+
+	interface IConsumer
+	{
+		Task<Event> ReadData();
+	}
+
+	interface IPublisher
+	{
+		Task<SendResult> SendData(Address address, Payload payload);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Task8
+{
+	internal class Program
+	{
+		private static ServiceProvider CreateServices()
+		{
+			var sp = new ServiceCollection();
+			sp.AddTransient<IClient, Client>();
+			sp.AddTransient<IHandler, Handler>();
+			sp.AddLogging(builder => builder.AddConsole());
+
+			return sp.BuildServiceProvider();
+		}
+
+		static async Task Main(string[] args)
+		{
+			var services = CreateServices();
+
+			var h = services.GetService<IHandler>();
+			var logger = services.GetService<ILogger<Program>>();
+
+			var res = await h.GetApplicationStatus(Guid.NewGuid().ToString("N"));
+			if (res is SuccessStatus ss)
+				logger.LogInformation("Success {Id}", ss.Id);
+			else if (res is FailureStatus fs)
+				logger.LogInformation("Failure {Time} {Count}", fs.LastRequestTime, fs.RetriesCount);
+		}
+	}
+}
